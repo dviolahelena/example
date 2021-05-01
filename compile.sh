4c4016186f9ee829fced91f5baa179ff75177819
Viola <deakviola@gmail.com>
2021-05-01 13:57:02 +0200

    changed content files to md

--- compile.sh
+++ compile.sh
@@ -2,8 +2,8 @@
 echo "MAKING THE OUT DIRECTORY"
 mkdir out
 echo "GENERATING ROLUNK" 
-pandoc -s --template template.html content/rolunk.html -o out/rolunk.html -M title="Példa-Rólunk"
+pandoc -s --template template.html content/rolunk.md -o out/rolunk.html -M title="Példa-Rólunk"
 echo "GENERATING KEZDOLAP"
-pandoc -s --template template.html content/kezdolap.html -o out/kezdolap.html -M title="Példa-Kezdőlap"
+pandoc -s --template template.html content/kezdolap.md -o out/kezdolap.html -M title="Példa-Kezdőlap"
 echo FINISHED
 
