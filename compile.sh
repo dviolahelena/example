1ebf94fc999064a720830133a58d899f9bbb71de
Viola <deakviola@gmail.com>
2021-05-22 12:52:33 +0200

    Renamed kezdolap to index

--- compile.sh
+++ compile.sh
@@ -4,6 +4,6 @@ mkdir out
 echo "GENERATING ROLUNK" 
 pandoc -s --template template.html content/rolunk.md -o out/rolunk.html -M title="Példa-Rólunk"
 echo "GENERATING KEZDOLAP"
-pandoc -s --template template.html content/kezdolap.md -o out/kezdolap.html -M title="Példa-Kezdőlap"
+pandoc -s --template template.html content/index.md -o out/index.html -M title="Példa-Kezdőlap"
 echo FINISHED
 
