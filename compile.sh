978644eb3f39df2c48cebcd3b12b7bb9be574338
Viola <deakviola@gmail.com>
2021-06-05 13:25:19 +0200

    MAKING MEDIA IN OUT

--- compile.sh
+++ compile.sh
@@ -5,5 +5,7 @@ echo "GENERATING ROLUNK"
 pandoc -s --template template.html content/rolunk.md -o out/rolunk.html
 echo "GENERATING KEZDOLAP"
 pandoc -s --template template.html content/index.md -o out/index.html
+echo "COPYING MEDIA FILES"
+cp -r media out/media
 echo FINISHED
 
